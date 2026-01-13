e8fae749bbe46a2e89865b59dede9343e0ede15f
SACHIN <sachinsaini7870@gmail.com>
2026-01-13 13:08:24 +0530

    add pagination functionality

--- ekart/store/views.py
+++ ekart/store/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from store.models import Product
 from category.models import Category
-from django.shortcuts import get_object_or_404
 from carts.models import CartItem
+
 from carts.views import _cart_id
-from django.http import HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 
 # Create your views here.
@@ -17,13 +18,19 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get("page")
+        paged_products = paginator.get_page(page)
         products_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get("page")
+        paged_products = paginator.get_page(page)
         products_count = products.count()
 
     context = {
-        "products": products,
+        "products": paged_products,
         "products_count": products_count,
     }
 
